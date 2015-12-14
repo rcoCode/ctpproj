2d7f0624442c468dd773f046c3ec2f74b26c5aa8
= <amily52131@aol.com>
2015-12-14 01:12:19 -0500

    global function creating List of ToolTypes upon starting app

--- app/global/global.java
+++ app/global/global.java
@@ -1,8 +1,13 @@
 package global;
 
+import models.ToolType;
 import models.Users;
 import play.GlobalSettings;
 import play.Application;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by yfle on 12/13/2015.
@@ -15,7 +20,13 @@ public class global extends GlobalSettings {
             admin.admin = true;
             admin.save();
         }
-
+        List<String> toolName = Arrays.asList("Power Tools", "Hand Tools", "Outdoor", "Interior", "Automotive");
+        for(String i:toolName){
+            if(ToolType.find.where().eq("name",i).findUnique()==null){
+                ToolType type = ToolType.createType(i);
+                type.save();
+            }
+        }
 
         super.onStart(application);
     }
