e43fdda28475851a24147a0230aa2cd63bda0d4a
Rebeca Otero <rebeca.otero2012@gmail.com>
2015-12-14 02:11:26 -0500

    Navbar update and basic user security

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -6,13 +6,20 @@ import play.mvc.Result;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import play.mvc.Security;
 import views.html.*;
 
 /**
  * Created by rebeca on 12/11/2015.
  */
 public class Users extends Controller{
+    @Security.Authenticated(UserAuth.class)
     public Result show(Long id){
+        if(Long.parseLong(session().get("user_id")) != id){
+            flash().put("error","Nice try, but that is not your profile!");
+            return redirect(routes.Application.index());
+        }
         models.Users auser = models.Users.find.byId(id);
         if(auser == null){
             return notFound("not found");
